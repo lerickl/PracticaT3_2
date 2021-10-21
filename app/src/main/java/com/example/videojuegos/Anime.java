16a00069e9d17f336633be477afd489a81b1948c
lerickl <nearerick@gmail.com>
2021-10-21 11:06:21 -0500

    first commit

---
+++ app/src/main/java/com/example/videojuegos/Anime.java
@@ -0,0 +1,15 @@
+package com.example.videojuegos;
+
+public class Anime {
+    public int avatar;
+    public String nombre;
+    public String Descripcion;
+    public boolean meGusta;
+
+    public Anime(int avatar, String nombre, String descripcion, boolean megusta) {
+        this.avatar = avatar;
+        this.nombre = nombre;
+        Descripcion = descripcion;
+        meGusta=megusta;
+    }
+}
